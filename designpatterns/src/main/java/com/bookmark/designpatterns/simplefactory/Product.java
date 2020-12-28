16489578e247310de0f33dbf13be729f06e23ece
jacobhuang <hj9667@outlook.com>
2020-12-28 16:52:16 +0800

    feat : 简单工厂实现

---
+++ designpatterns/src/main/java/com/bookmark/designpatterns/simplefactory/Product.java
@@ -0,0 +1,9 @@
+package com.bookmark.designpatterns.simplefactory;
+
+/**
+ * @author: hj
+ * @date: 2020-12-26 18:53
+ * @description:
+ **/
+public interface Product {
+}
---
+++ designpatterns/src/main/java/com/bookmark/designpatterns/simplefactory/ProductImpl3.java
@@ -0,0 +1,10 @@
+package com.bookmark.designpatterns.simplefactory;
+
+/**
+ * @author: hj
+ * @date: 2020-12-26 18:54
+ * @description:
+ **/
+public class ProductImpl3 implements Product {
+
+}
