f16163223301b7116ab0498779c018f608725f71
Jacobhuang <hj9667@outlook.com>
2021-02-24 23:45:23 +0800

    简单队列的实现---周三晚上未敲完,需要注意时间

--- algorithm/src/main/java/com/bookmark/datastructure/ArrayQueueDemo.java
+++ algorithm/src/main/java/com/bookmark/datastructure/ArrayQueueDemo.java
@@ -11,24 +11,43 @@ public class ArrayQueueDemo {
     /**
      * 定义队列
      */
-    class Queue {
-        //头元素坐标
+    class ArrayQueue {
+        //队列第一个元素的前一个位置，初始的时候为-1
         private int front;
-        //尾元素坐标
+        //队列最后一个元素的下标
         private int rear;
-        //实际队列
+        //队列大小
+        private int size;
+        //模拟队列的数组
         private int[] arr;
 
+        //构造方法
+        public ArrayQueue(int size) {
+            this.arr = new int[size];
+            this.front = -1;
+            this.rear = -1;
+        }
+
         //判断队列是否为空
+        public boolean isEmpty() {
+            return front == rear;
+        }
 
 
         //判断队列是否已满
+        public boolean isFull() {
+            return false;
+        }
 
         //添加元素
 
         //取出元素
 
         //查看第一个元素
+
+        //查看队列中所有的有效元素
+
+
 
 
     }
