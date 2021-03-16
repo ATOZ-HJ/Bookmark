4553022cb08d0b63b65660b5b3530252704927ac
jacobhuang <hj9667@outlook.com>
2021-03-16 08:52:54 +0800

    1.二维数组算法题 2.最近计划修改

---
+++ algorithm/src/main/java/com/datastructure/Test.java
@@ -0,0 +1,28 @@
+package com.datastructure;
+
+import java.util.List;
+
+/**
+ * @author: hj
+ * @date: 2021-03-13 15:14
+ * @description:
+ *
+ *
+ * 二维数组：
+ * 需求：有数组[["A","a","w"],["B","b"],["1","2"]]，组装所有随机字符形成字符串，保存到数组中输出数组
+ * 如：AB1、AB2、Ab1、Ab2、aB1、aB2、ab1、ab2
+ *
+ **/
+public class Test {
+    public static void main(String[] args) {
+        String[][] arr = {{"A", "a", "w"}, {"B", "b"}, {"1", "2"}};
+        transferArr(arr);
+    }
+
+    private static List<String> transferArr(String[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        return null;
+    }
+}
