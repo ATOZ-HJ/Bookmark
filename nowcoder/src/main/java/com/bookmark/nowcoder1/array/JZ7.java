80c294811ddee565d4f81b63ee7d45f8e613db48
jacobhuang <jian.huang@kingstarfintech.com>
2021-06-16 09:02:09 +0800

    feat: 提交牛客网算法题

--- nowcoder/src/main/java/com/bookmark/nowcoder1/array/JZ7.java
+++ nowcoder/src/main/java/com/bookmark/nowcoder1/array/JZ7.java
@@ -15,14 +15,17 @@ public class JZ7 {
             return n;
         }
 
-        int[] array = new int[39];
-        array[0] = 0;
-        array[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            array[i] = array[i - 1] + array[i - 2];
-        }
+        if (n <= 39) {
+            int[] array = new int[n - 1];
+            array[0] = 0;
+            array[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            return array[n];
 
-        return array[n];
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
