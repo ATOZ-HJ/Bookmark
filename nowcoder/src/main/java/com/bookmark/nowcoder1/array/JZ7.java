f0201d3c8a9595beb57a81ccc6c96d0bedb21c26
jacobhuang <jian.huang@kingstarfintech.com>
2021-06-22 19:51:54 +0800

    提交算法题和开发测试以及梳理

--- nowcoder/src/main/java/com/bookmark/nowcoder1/array/JZ7.java
+++ nowcoder/src/main/java/com/bookmark/nowcoder1/array/JZ7.java
@@ -10,22 +10,45 @@ package com.bookmark.nowcoder1.array;
  **/
 
 public class JZ7 {
+
+    /**
+     * 运行时间：12ms
+     * 超过79.52% 用Java提交的代码
+     * 占用内存：9720KB
+     * 超过5.00%用Java提交的代码
+     *
+     * @param n
+     * @return
+     */
     public static int Fibonacci(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
-
-        if (n <= 39) {
-            int[] array = new int[n - 1];
-            array[0] = 0;
-            array[1] = 1;
-            for (int i = 2; i <= n; i++) {
-                array[i] = array[i - 1] + array[i - 2];
-            }
-            return array[n];
-
+        int[] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2];
         }
-        return 0;
+        return array[n];
+    }
+
+    /**
+     * 官方解答1:使用递归实现，时间复杂度较高
+     * 运行时间：323ms
+     * 超过33.92% 用Java提交的代码
+     * 占用内存：9764KB
+     * 超过3.38%用Java提交的代码
+     *
+     * @param n
+     * @return
+     */
+    public static int Fibonacci1(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return Fibonacci1(n - 1) + Fibonacci1(n - 2);
     }
 
     public static void main(String[] args) {
