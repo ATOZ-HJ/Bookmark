a088d809290ddb8f005b352d202b4c574f8ecd20
jacobHuang <ling.xu@kingstarfintech.com>
2021-08-26 23:55:58 +0800

    JZ52 算法题

---
+++ algorithm/src/main/java/com/datastructure/nowcoder/array/JZ52.java
@@ -0,0 +1,25 @@
+package com.datastructure.nowcoder.array;
+
+/**
+ * @author hj
+ * @date 2021-08-26 11:26 下午
+ * @description 描述：给定一个数组A[0, 1, ..., n-1],请构建一个数组B[0,1,...,n-1],
+ * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+ * 注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，
+ * B[n-1] = A[0] * A[1] * ... * A[n-2];）
+ * 对于A长度为1的情况，B无意义，故而无法构建，因此该情况不会存在。
+ * <p>
+ * 示例：
+ * 输入：
+ * [1,2,3,4,5]
+ * 返回值：
+ * [120,60,40,30,24]
+ */
+public class JZ52 {
+    public int[] multiply(int[] arr) {
+        int[] arrB = new int[arr.length];
+        for (int i = 0; i < arrB.length; i++) {
+            
+        }
+    }
+}
