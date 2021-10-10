9c3adfd6c319dbd8e551be4ea92cac2e735ccaf0
Jacobhuang <hj9667@outlook.com>
2021-10-10 23:50:57 +0800

    提交项目描述介绍和模板

--- developtest/src/main/java/com/book/developtest/DevTest.java
+++ developtest/src/main/java/com/book/developtest/DevTest.java
@@ -1,15 +1,13 @@
 package com.book.developtest;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
-
-import com.book.developtest.util.JsonUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import com.book.developtest.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: hj
@@ -30,9 +28,9 @@ public class DevTest {
     @Test
     public void test2() {
         String json = "{\n" + "    \"task_id\": \"xx123s45a3\",\n" + "    \"retcode\": 0,\n" + "    \"retmsg\": \"\",\n"
-            + "    \"variety_list\":[{\"leg1_symbol\":\"Au(T+D)\"}, {\"leg2_symbol\":\"Ag(T+D)\"},{\"leg2_symbol\":\"Au99.99\"}],\n"
-            + "\t\"parameters\":[{\"n_up\":2}, {\"n_down\":0}, {\"n_mid\":0}, {\"f_up\":0}, {\"f_down\":0}, {\"f_mid\":0}]\n"
-            + "}";
+                + "    \"variety_list\":[{\"leg1_symbol\":\"Au(T+D)\"}, {\"leg2_symbol\":\"Ag(T+D)\"},{\"leg2_symbol\":\"Au99.99\"}],\n"
+                + "\t\"parameters\":[{\"n_up\":2}, {\"n_down\":0}, {\"n_mid\":0}, {\"f_up\":0}, {\"f_down\":0}, {\"f_mid\":0}]\n"
+                + "}";
         System.out.println("json = " + json);
     }
 
@@ -63,7 +61,7 @@ public class DevTest {
     @Test
     public void test4() throws JsonProcessingException {
         String s =
-            "{\"task_id\": \"1435440689503670272\", \"timestamp\": \"2021-09-08 11:11:44.556130\", \"loglevel\": \"INFO\", \"logseq\": 0, \"logtype\": \"\\u7cfb\\u7edf\\u65e5\\u5fd7\", \"logmsg\": \"\\u4efb\\u52a1\\u5f00\\u59cb\\u6267\\u884c\", \"endflag\": false}";
+                "{\"task_id\": \"1435440689503670272\", \"timestamp\": \"2021-09-08 11:11:44.556130\", \"loglevel\": \"INFO\", \"logseq\": 0, \"logtype\": \"\\u7cfb\\u7edf\\u65e5\\u5fd7\", \"logmsg\": \"\\u4efb\\u52a1\\u5f00\\u59cb\\u6267\\u884c\", \"endflag\": false}";
         LogDTO logDTO = JsonUtil.toBean(s, LogDTO.class);
         System.out.println(logDTO);
     }
@@ -71,14 +69,14 @@ public class DevTest {
     @Test
     public void test5() {
         String s =
-            "{\"task_id\": \"1631093013xx123s45a37474\", \"status\": \"SUCCESS\", \"info\": \"\", \"progress\": 1, \"seq\": 1, \"variety_list\": {\"leg1_symbol\": \"Ag(T+D)\", \"leg2_symbol\": \"Au(T+D)\"}, \"parameters\": {\"ma\": 10, \"n\": 2}}";
+                "{\"task_id\": \"1631093013xx123s45a37474\", \"status\": \"SUCCESS\", \"info\": \"\", \"progress\": 1, \"seq\": 1, \"variety_list\": {\"leg1_symbol\": \"Ag(T+D)\", \"leg2_symbol\": \"Au(T+D)\"}, \"parameters\": {\"ma\": 10, \"n\": 2}}";
         BizStrategyPreCompileResDTO bizStrategyPreCompileResDTO = JsonUtil.toBean(s, BizStrategyPreCompileResDTO.class);
         System.out.println("bizStrategyPreCompileResDTO = " + bizStrategyPreCompileResDTO);
     }
 
     @Test
     public void test6() {
-
+        System.out.println(98 % 4);
     }
 
 }
