67c1a8ba6fa3f4d9c2362a74c7736e7cd88b3364
jacobHuang <ling.xu@kingstarfintech.com>
2021-11-17 23:45:37 +0800

    两数之和和三数之和题目提交

---
+++ algorithm/src/main/java/com/bookmark/algo/camp/Leetcode01.java
@@ -0,0 +1,66 @@
+package com.bookmark.algo.camp;
+
+import java.util.Arrays;
+
+/**
+ * @author hj
+ * @date 2021-11-17 11:26 下午
+ * @description 两数之和
+ * <p>
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leetcode01 {
+    /**
+     * 1.暴力求解，双层for循环去找出所有的组合
+     * 2.使用map缓存
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int[] ints = twoSum(arr, 9);
+        System.out.println("ints = " + Arrays.toString(ints));
+
+
+    }
+}
