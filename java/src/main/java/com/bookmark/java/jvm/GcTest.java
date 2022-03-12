b2b19de14ae286aba22700f42552a1beac7d47c1
jacobHuang <ling.xu@kingstarfintech.com>
2022-03-12 20:47:05 +0800

    jvm相关笔记提交

---
+++ java/src/main/java/com/bookmark/java/jvm/GcTest.java
@@ -0,0 +1,16 @@
+package com.bookmark.java.jvm;
+
+/**
+ * @author hj
+ * @date 2022-03-12 8:01 下午
+ * @description
+ */
+public class GcTest {
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[51536 * 1024];
+        allocation2 = new byte[21536 * 1024];
+        System.out.println("方法执行了");
+
+    }
+}
